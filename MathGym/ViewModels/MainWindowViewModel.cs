bdeeef090dcd72addd3a2868698987e13404be7b
Monty <montyj.tech@gmail.com>
2025-02-02 23:36:02 +0000

    Set template UI for MathGym

--- MathGym/ViewModels/MainWindowViewModel.cs
+++ MathGym/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace MathGym.ViewModels;
+﻿using Avalonia.Controls;
+using System.Reactive;
+using ReactiveUI;
+
+namespace MathGym.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia! This is my added text";
+    private Window window;
+
+    public ReactiveCommand<Unit, Unit> minimizeCommand { get; set;  }
+    public ReactiveCommand<Unit, Unit> maximizeCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> closeCommand { get; set; }
+
+    public MainWindowViewModel(Window window)
+    {
+        this.window = window;
+
+        minimizeCommand = ReactiveCommand.Create(() =>
+        {
+            this.window.WindowState = WindowState.Minimized;
+        });
+
+        maximizeCommand = ReactiveCommand.Create(() =>
+        {
+            this.window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        });
+
+        closeCommand = ReactiveCommand.Create(() =>
+        {
+            this.window.Close();
+        });
+    }
 }
--- MathGym/Views/MainWindow.axaml.cs
+++ MathGym/Views/MainWindow.axaml.cs
@@ -1,6 +1,5 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using System.Diagnostics;
+using MathGym.ViewModels;
 
 namespace MathGym.Views;
 
@@ -9,38 +8,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel(this);
+        DragArea.PointerPressed += (s, e) => BeginMoveDrag(e);
     }
-
-    //private void Button_OnClick(object? sender, RoutedEventArgs e)
-    //{
-    //    if(double.TryParse(Celcius.Text, out double C))
-    //    {
-    //        double F = C * (9d / 5d) + 32;
-    //        Fahrenheit.Text = F.ToString("0.0");
-    //    }
-    //    else
-    //    {
-    //        Fahrenheit.Text = "0";
-    //        Celcius.Text = "0";
-    //    }
-    //    Debug.WriteLine($"Clicked! Celcius: {Celcius.Text} - " + Celcius.Text);
-    //}
-
-    //private void Text_Changed(object? sender, TextChangedEventArgs e)
-    //{
-    //    if (double.TryParse(Celcius.Text, out double C))
-    //    {
-    //        double F = C * (9d / 5d) + 32;
-    //        Fahrenheit.Text = F.ToString("0.0");
-    //    }
-    //    else if (string.IsNullOrEmpty(Celcius.Text))
-    //    {
-    //        Fahrenheit.Text = "";
-    //    }
-    //    else
-    //    {
-    //        Fahrenheit.Text = "0";
-    //        Celcius.Text = "0";
-    //    }
-    //}
 }
