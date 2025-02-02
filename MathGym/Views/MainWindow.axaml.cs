eae3ab3a5426e3652624cfead72b1246daad1108
Monty <montyj.tech@gmail.com>
2025-02-02 19:14:05 +0000

    Finish test project

--- MathGym/Views/MainWindow.axaml.cs
+++ MathGym/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace MathGym.Views;
 
@@ -7,5 +9,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if(double.TryParse(Celcius.Text, out double C))
+        {
+            double F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Fahrenheit.Text = "0";
+            Celcius.Text = "0";
+        }
+        Debug.WriteLine($"Clicked! Celcius: {Celcius.Text} - " + Celcius.Text);
+    }
+
+    private void Text_Changed(object? sender, TextChangedEventArgs e)
+    {
+        if (double.TryParse(Celcius.Text, out double C))
+        {
+            double F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else if (string.IsNullOrEmpty(Celcius.Text))
+        {
+            Fahrenheit.Text = "";
+        }
+        else
+        {
+            Fahrenheit.Text = "0";
+            Celcius.Text = "0";
+        }
     }
 }
