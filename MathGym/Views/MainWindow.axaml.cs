d625424e8609d98548140c7001e25cb7271ee05d
Monty <montyj.tech@gmail.com>
2025-02-02 21:09:45 +0000

    New dragbar template UI

--- MathGym/Views/MainWindow.axaml.cs
+++ MathGym/Views/MainWindow.axaml.cs
@@ -11,36 +11,36 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if(double.TryParse(Celcius.Text, out double C))
-        {
-            double F = C * (9d / 5d) + 32;
-            Fahrenheit.Text = F.ToString("0.0");
-        }
-        else
-        {
-            Fahrenheit.Text = "0";
-            Celcius.Text = "0";
-        }
-        Debug.WriteLine($"Clicked! Celcius: {Celcius.Text} - " + Celcius.Text);
-    }
+    //private void Button_OnClick(object? sender, RoutedEventArgs e)
+    //{
+    //    if(double.TryParse(Celcius.Text, out double C))
+    //    {
+    //        double F = C * (9d / 5d) + 32;
+    //        Fahrenheit.Text = F.ToString("0.0");
+    //    }
+    //    else
+    //    {
+    //        Fahrenheit.Text = "0";
+    //        Celcius.Text = "0";
+    //    }
+    //    Debug.WriteLine($"Clicked! Celcius: {Celcius.Text} - " + Celcius.Text);
+    //}
 
-    private void Text_Changed(object? sender, TextChangedEventArgs e)
-    {
-        if (double.TryParse(Celcius.Text, out double C))
-        {
-            double F = C * (9d / 5d) + 32;
-            Fahrenheit.Text = F.ToString("0.0");
-        }
-        else if (string.IsNullOrEmpty(Celcius.Text))
-        {
-            Fahrenheit.Text = "";
-        }
-        else
-        {
-            Fahrenheit.Text = "0";
-            Celcius.Text = "0";
-        }
-    }
+    //private void Text_Changed(object? sender, TextChangedEventArgs e)
+    //{
+    //    if (double.TryParse(Celcius.Text, out double C))
+    //    {
+    //        double F = C * (9d / 5d) + 32;
+    //        Fahrenheit.Text = F.ToString("0.0");
+    //    }
+    //    else if (string.IsNullOrEmpty(Celcius.Text))
+    //    {
+    //        Fahrenheit.Text = "";
+    //    }
+    //    else
+    //    {
+    //        Fahrenheit.Text = "0";
+    //        Celcius.Text = "0";
+    //    }
+    //}
 }
