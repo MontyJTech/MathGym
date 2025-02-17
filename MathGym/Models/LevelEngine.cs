2fb3258f7af299246f5f31db4c1859ca65dfa954
Monty <montyj.tech@gmail.com>
2025-02-17 23:25:44 +0000

    wip

---
+++ MathGym/Models/LevelEngine.cs
@@ -0,0 +1,26 @@
+﻿class LevelEngine
+{
+    public const int NUM_LEVELS = 10;
+    public int curLevel = 0;
+
+    public void BeginLevel()
+    {
+        //Start a timer
+    }
+
+    public void LevelStep()
+    {
+        curLevel++;
+    }
+
+    public bool CheckLevelEnd()
+    {
+        return curLevel < NUM_LEVELS;
+    }
+
+    public void EndLevel() 
+    { 
+        //Stop the timer
+        //Log the time
+    }
+}
